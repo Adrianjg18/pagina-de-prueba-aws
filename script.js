35bb7d54cea6652bd9d3e0897dbdcee60c0813d0
Adrian Gomez <127125334+Adrianjg18@users.noreply.github.com>
2024-11-15 16:47:11 -0400

    Update script.js ip server

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const ipServidor = document.getElementById("ip-server");
 const nombreServidor = document.getElementById("name-server");
 
-fetch("/server-ip")
+fetch("http://169.254.169.254/latest/meta-data/public-ipv4")
   .then((response) => response.json())
   .then((data) => {
     ipServidor.textContent = data.ip;
