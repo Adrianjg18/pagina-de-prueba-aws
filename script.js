bc48439b16501708ec463bcf35ae152908c2e9fc
Adrian Gomez <127125334+Adrianjg18@users.noreply.github.com>
2024-11-15 16:36:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const servidor = document.getElementById('servidor');
+const ipServidor = document.getElementById("ip-server");
+const nombreServidor = document.getElementById("name-server");
+
+fetch("https://api.ipify.org?format=json")
+  .then((response) => response.json())
+  .then((data) => {
+    ipServidor.textContent = data.ip;
+  })
+  .catch((error) => console.error("Error obteniendo la IP:", error));
+
+nombreServidor.textContent = window.location.hostname;
